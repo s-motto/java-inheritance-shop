a77fd9b4f0157f7c1c26c285327a326e1e76d79b
s-motto <brinamotto@gmail.com>
2024-06-27 00:26:17 +0200

    add class cuffie

--- Cuffie.java
+++ Cuffie.java
@@ -18,6 +18,13 @@ public class Cuffie extends Prodotto{
 		this.wirelessOCablate = wirelessOCablate;
 	}
 	
-	
+	public Cuffie(int codice, String nome, String marca, double prezzo, int iva, String colore, String wirelessOCablate){
+		super(codice,nome,marca,prezzo,iva);
+		this.colore=colore;
+		this.wirelessOCablate=wirelessOCablate;
+	}
 
+	String infoCuffie(){
+		 return "Codice "+getCodice()+", "+nome+", "+marca+", "+ colore +",  "+ wirelessOCablate+", "+prezzo+"euro, iva al "+iva+"%"+"."+" Prezzo finale: "+prezzoPiuIva();
+		 }
 }
