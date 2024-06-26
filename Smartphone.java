16f8430cfe1fa362b2b169632be41e6a77dd1d3d
s-motto <brinamotto@gmail.com>
2024-06-27 00:25:47 +0200

    add class smartphone

--- Smartphone.java
+++ Smartphone.java
@@ -4,7 +4,6 @@ public class Smartphone extends Prodotto {
 	private int memoria;
 	
 	
-	
 	//getter e setter
 	public int getImei() {
 		return imei;
@@ -20,6 +19,13 @@ public class Smartphone extends Prodotto {
 		this.memoria = memoria;
 	}
 	
+	public Smartphone(int codice, String nome, String marca, double prezzo, int iva, int imei, int memoria){
+		super(codice,nome,marca,prezzo,iva);
+		this.imei=imei;
+		this.memoria=memoria;
+	}
 	
-	
-}
+	 String infoSmartphone(){
+		 return "Codice "+getCodice()+", "+nome+", "+marca+", "+ "IMEI "+imei+", memoria "+memoria+" GB, "+prezzo+"euro, iva al "+iva+"%"+"."+" Prezzo finale: "+prezzoPiuIva();
+		 };
+	}
